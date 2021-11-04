e3a6674eabe4ff590d21bdc62e42fa64ec4f31b9
zenquan <1847867150@qq.com>
2021-11-04 22:39:27 +0800

    feat: 初步完成七牛云上传单个文件

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -4,14 +4,11 @@ import {
 } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import fs from 'fs'
 import { upyunPlugin, qiniuPlugin } from '../src/index'
 
 // https://vitejs.dev/config/
 export default ({ command, mode }: ConfigEnv): UserConfig => {
   const isBuild = command === 'build';
-
-  console.log('QINIU_ACCESSKEY>>>', process.env.QINIU_ACCESSKEY);
 
   return {
     plugins: [
@@ -24,8 +21,8 @@ export default ({ command, mode }: ConfigEnv): UserConfig => {
       //   filePath: path.resolve(process.cwd(), 'dist/assets')                  
       // })
       isBuild && qiniuPlugin({
-        accessKey: process.env.QINIU_ACCESSKEY || 'UYJ5ZYRaK3qdltRETZ23wBxC1E3toWsJyp2HwmOW',
-        secretKey: process.env.QINIU_SECRETKEY || 'm8-W2xlvY2c4CNop8_6UKYeR3DAjmAJpdIID1n8j',
+        accessKey: process.env.QINIU_ACCESSKEY || '',
+        secretKey: process.env.QINIU_SECRETKEY || '',
         scope: 'zeditor',
         file: path.resolve(__dirname, './dist/index.html'),
         fileName: 'index.html'
